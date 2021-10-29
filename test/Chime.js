932a5dcc6590b3edebcb99bbee2663248c2b9490
ALegendsTale <1750241-ALegendsTale@users.noreply.gitlab.com>
2021-10-29 00:01:03 -0400

    Implemented friend/block list functionality
    Can now add friends and blocked contracts to list
    
    Removed temporary code

--- test/Chime.js
+++ test/Chime.js
@@ -1,2 +1,38 @@
 const { expect } = require("chai");
 
+describe("Chime", function () {  
+    let chime;
+    let chimeContract;
+    let addr1 = "0x09750ad360fdb7a2ee23669c4503c974d86d8694";
+    let addr2 = "0xc915eC7f4CFD1C0A8Aba090F03BfaAb588aEF9B4";
+    let addr3 = "0xecb6ffaC05D8b4660b99B475B359FE454c77D153";
+    let addr4 = "0x7F85A82a2da50540412F6E526F1D00A0690a77B8";
+
+    beforeEach(async function () {
+        chime = await ethers.getContractFactory("Chime");
+        chimeContract = await chime.deploy();
+
+        chimeContract.addToList(addr1, "John", 1);
+        chimeContract.addToList(addr2, "Samantha", 1);
+        chimeContract.addToList(addr3, "Robert", 2);
+        chimeContract.addToList(addr4, "Lynn", 2);
+    });
+
+        describe("Add friends", function () {
+            it("should add a friend", async function() {
+                expect(await chimeContract.getName(addr3)).equal("Robert");
+                expect(await chimeContract.getName(addr1)).equal("John");
+            });
+        });
+        
+        describe("Check relationship", function () {
+            it("relationship should be assigned", async function() {
+                //Check if friend
+                expect(await chimeContract.getRelationship(addr2)).equal(1);
+                //Check if blocked
+                expect(await chimeContract.getRelationship(addr4)).equal(2);
+            });
+        });
+    
+        //End
+    });
