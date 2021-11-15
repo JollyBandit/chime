52bad273df54ccb03c7a1356d44dfaa04064d788
ALegendsTale <1750241-ALegendsTale@users.noreply.gitlab.com>
2021-11-14 20:23:56 -0500

    Add Whitepaper & Chainlink Feed Data
    + Chime Whitepaper
    + Chainlink Data Feed on button click

--- front-end/src/components/SendMessage.jsx
+++ front-end/src/components/SendMessage.jsx
@@ -1,7 +1,11 @@
 import React, { useState } from "react";
+require("dotenv").config();
+
+const API_KEY = process.env.REACT_APP_INFURA_API_KEY;
 
 export const SendMessage = ({ sendMessage }) => {
   const [inputValue, setInputValue] = useState("");
+  const { ethers } = require("ethers")
 
   const keyHandler = (event) => {
     if (event.key === "Enter" && inputValue.trim() !== "") {
@@ -21,6 +25,67 @@ export const SendMessage = ({ sendMessage }) => {
     }
   };
 
+  const priceFeed = () => {
+    const provider = new ethers.providers.JsonRpcProvider("https://kovan.infura.io/v3/08b75c4569f541e4835952ffa1f10640");
+    const aggregatorV3InterfaceABI = [
+      {
+        inputs: [],
+        name: "decimals",
+        outputs: [{ internalType: "uint8", name: "", type: "uint8" }],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "description",
+        outputs: [{ internalType: "string", name: "", type: "string" }],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [{ internalType: "uint80", name: "_roundId", type: "uint80" }],
+        name: "getRoundData",
+        outputs: [
+          { internalType: "uint80", name: "roundId", type: "uint80" },
+          { internalType: "int256", name: "answer", type: "int256" },
+          { internalType: "uint256", name: "startedAt", type: "uint256" },
+          { internalType: "uint256", name: "updatedAt", type: "uint256" },
+          { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "latestRoundData",
+        outputs: [
+          { internalType: "uint80", name: "roundId", type: "uint80" },
+          { internalType: "int256", name: "answer", type: "int256" },
+          { internalType: "uint256", name: "startedAt", type: "uint256" },
+          { internalType: "uint256", name: "updatedAt", type: "uint256" },
+          { internalType: "uint80", name: "answeredInRound", type: "uint80" },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "version",
+        outputs: [{ internalType: "uint256", name: "", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+      },
+    ];
+    const addr = "0x9326BFA02ADD2366b30bacB125260Af641031331";
+    const priceFeed = new ethers.Contract(addr, aggregatorV3InterfaceABI, provider);
+    priceFeed.latestRoundData()
+        .then((roundData) => {
+            // Do something with roundData
+            console.log("Latest Round Data", roundData)
+        })
+
+  }
+
   return (
     <section id="send-message">
       <button id="add-file" onClick={() => console.log("Add File")}>+</button>
@@ -35,7 +100,7 @@ export const SendMessage = ({ sendMessage }) => {
         onInput={(e) => setInputValue(e.target.value)}
         onKeyPress={(e) => keyHandler(e)}
       ></input>
-      <button id="chainlink-feeds" onClick={() => console.log("Open pricefeed selection")}>&#x2B21;</button>
+      <button id="chainlink-feeds" onClick={() => priceFeed()}>&#x2B21;</button>
       <button id="pick-emoji" onClick={() => console.log("Pick emoji")}>&#x1F60A;</button>
       <button id="message-submit" onClick={(e) => buttonHandler(e)}>Submit</button>
     </section>
